19c10e9a256366481e3cf7c8649c71e25b5a7d3b
xiqingliu <546056884@qq.com>
2016-09-07 18:36:15 +0800

    权限

--- app/src/main/java/com/alenbeyond/androidutils/utils/AppInfoUtil.java
+++ app/src/main/java/com/alenbeyond/androidutils/utils/AppInfoUtil.java
@@ -19,4 +19,25 @@ public class AppInfoUtil {
         }
         return info.activityInfo.name;
     }
+	
+	public static boolean checkPermissionGranted(Context context, String permission) {
+        boolean result = true;
+        int targetSdkVersion = 0;
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            targetSdkVersion = info.applicationInfo.targetSdkVersion;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (targetSdkVersion >= Build.VERSION_CODES.M) {
+                result = context.checkSelfPermission(permission)
+                        == PackageManager.PERMISSION_GRANTED;
+            } else {
+                result = PermissionChecker.checkSelfPermission(context, permission)
+                        == PermissionChecker.PERMISSION_GRANTED;
+            }
+        }
+        return result;
+    }
 }
